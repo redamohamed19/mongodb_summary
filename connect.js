f2694a90c00a3cd54a71634dd280da4bf52fbd5e
Sayah Reda <reda1998.rs@gmail.com>
2022-04-18 23:47:53 +0100

    Basic Query Methods

--- connect.js
+++ connect.js
@@ -16,12 +16,18 @@ const food=new mongoose.Schema({
         price:String
     },
     Categorie:[{type:String}]
+    
    
 
-})
+},{timestamps:true})
 const Food=mongoose.model("food",food);
 
 connect().then(async connection =>{
-    const food=await Food.create({name:"duchi bochi",time:"20min 40 min",price:'25$'})
-    console.log(food)
+    const food=await Food.create({name:"pollos kenllas",info:{time:"30min 50 min",price:'20$'},Categorie:["burger"]})
+    const found=await Food.find({name:"pollos mollos"})
+    const foundbyId=await Food.findById('625de8c18b84700dda10869a')
+    const updated=await Food.findByIdAndUpdate('625de8c18b84700dda10869a',{name:"kerasi"})
+
+    console.log(found)
+    console.log(foundbyId)
 }).catch(e=>{console.error(e)})
