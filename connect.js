c3448c9760da828229669e7177839f65d2bbceb6
Sayah Reda <reda1998.rs@gmail.com>
2022-04-18 23:10:44 +0100

    Creating a Mongo Document

--- connect.js
+++ connect.js
@@ -5,10 +5,15 @@ const connect=()=>{
     return mongoose.connect("mongodb://localhost:27017/delivery")
 }
 const food=new mongoose.Schema({
-    fetured:boolean,
+ 
     name:String,
     time:String,
     price:String
 
 })
 const Food=mongoose.model("food",food);
+
+connect().then(async connection =>{
+    const food=await Food.create({name:"duchi bochi",time:"20min 40 min",price:'25$'})
+    console.log(food)
+}).catch(e=>{console.error(e)})
